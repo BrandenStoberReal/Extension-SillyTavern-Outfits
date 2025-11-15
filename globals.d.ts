792f7a10db302c6c6ee32c566004dea270872bbb
Amnesia <amnesia@example.com>
2025-11-14 21:46:27 -0500

    Update submodule

--- globals.d.ts
+++ globals.d.ts
@@ -2,8 +2,9 @@ export {};
 
 // 1. Import when extension is user-scoped
 import '../../../../public/global';
+
 // 2. Import when extension is server-scoped
-import '../../../../global';
+// import '../../../../global';
 
 declare global {
     // Add global type declarations here
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import './style.css';
-import message from './index.html';
+import message from './HTML/index.html';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const { Popup } = (globalThis as any).SillyTavern.getContext();
