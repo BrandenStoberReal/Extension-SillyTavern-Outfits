6518ceff2129faf2e4bcf55ef814c5c2bb32085f
amnesia <amnesia@example.com>
2025-11-20 11:51:49 -0500

    Prevent stupid default popup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import './style.css';
-import message from './html/index.html';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const {Popup} = SillyTavern.getContext();
 
-Popup.show.text(message);
+// Popup.show.text(message);
